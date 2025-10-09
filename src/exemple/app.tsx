4d33249a0e822d171e752245191daa70e08347b0
Jose Luiz Rodrigues <zeluizr@me.com>
2025-10-09 20:44:40 -0300

    Refactor application structure: replace main component, remove unused styles and type declarations
    - Replaced the main application component with a new App component from the exemple directory.
    - Removed the main CSS file as it was no longer needed.
    - Deleted the type declarations for various file formats as they are not in use.
    - Introduced a new App component that currently renders a secondary button.

---
+++ src/exemple/app.tsx
@@ -0,0 +1,5 @@
+import { Button } from "../components";
+
+export default function App() {
+  return <Button variant="secondary">Botão Secundário</Button>;
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,4 @@
-import { Button } from "@searchmindai/ui";
 import { render } from "preact";
-import "./styles/main.css";
-
-function App() {
-  return (
-    <div>
-      <h1>Testando Componentes</h1>
-      <Button onClick={() => alert("Clicou!")}>Botão Primário</Button>
-      <Button variant="secondary">Botão Secundário</Button>
-    </div>
-  );
-}
+import App from "./exemple/app";
 
 render(<App />, document.getElementById("app")!);
